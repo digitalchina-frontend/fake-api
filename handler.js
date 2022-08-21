5b45a87b494417e78b3673003ed1c1fa6518e29f
fancn21th <fancn21th@gmail.com>
2022-08-21 13:51:48 +0800

    leverage fake api

--- handler.js
+++ handler.js
@@ -1,12 +1,18 @@
 "use strict";
+const { faker } = require("@faker-js/faker");
+
+faker.setLocale("zh_CN");
 
 module.exports.fakeApi = async (event) => {
+  const randomName = faker.name.fullName(); // Rowan Nikolaus
+  const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        randomName,
+        randomEmail,
       },
       null,
       2
