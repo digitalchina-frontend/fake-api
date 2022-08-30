69f8d8b854871e94eb64330295814941c7dcd003
fancn21th <fancn21th@gmail.com>
2022-08-30 15:50:13 +0800

    new fake api  remove fakejs

--- handler.js
+++ handler.js
@@ -1,18 +1,13 @@
 "use strict";
-const { faker } = require("@faker-js/faker");
 
-faker.setLocale("zh_CN");
-
-module.exports.fakeApi = async (event) => {
-  const randomName = faker.name.fullName(); // Rowan Nikolaus
-  const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+module.exports.fake = async (event) => {
+  const path = event.pathParameters.proxy;
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        randomName,
-        randomEmail,
+        path,
       },
       null,
       2
